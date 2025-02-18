4d552b6efa0bd8a762fadca0d5a9d78fb29ccaec
mahdi-ghafarian <mahdi.ghaf@outlook.com>
2025-02-18 12:41:10 -0600

    new file:   tourism/__pycache__/helper.cpython-313.pyc 	modified:   tourism/data/data.md 	new file:   tourism/helper.py 	modified:   tourism/main.ipynb

--- tourism/data/data.md
+++ tourism/data/data.md
@@ -4,6 +4,9 @@
 Frequency: Quarterly
 
 [24-10-0045-01](https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2410004501): Travel by Canadian residents in Canada and abroad by trip purpose
+* Trip duration
+* Trip purpose
+* Destination: Canada, US, Overseas
 
 ## [Visitor Travel Survey ](https://www150.statcan.gc.ca/n1/daily-quotidien/241128/dq241128c-cansim-eng.htm)
 
@@ -16,10 +19,21 @@ Frequency: Quarterly
 
  [24-10-0066-01](https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2410006601): Visits, nights and spending for visitors to Canada by geography of visit, residency and mode of transport
 
+## [Frontier Counts (FC)](https://www23.statcan.gc.ca/imdb/p2SV.pl?Function=getSurvey&SDDS=5005)
+
+Frequency: Daily
+
+24-10-0056-01
+
+24-10-0057-01
+
+24-10-0058-01
+
+24-10-0059-01
 
 ## [Travel between Canada and other countries](https://www150.statcan.gc.ca/n1/daily-quotidien/250123/dq250123c-cansim-eng.htm)
 
-Frequency: Monthly
+Frequency: Monthly (aggregated from frontier counts)
 
 [24-10-0050-01](https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2410005001): Non-resident visitors entering Canada, by country of residence
 
---
+++ tourism/helper.py
@@ -0,0 +1,61 @@
+## Create DataFrame from Statistics Canada Table
+
+import pandas as pd
+import urllib.parse
+
+# This script demonstrates how to download a CSV file from the Statistics Canada website
+# using the downloadDbLoadingData-nonTraduit.action endpoint.
+def create_df_sc(pid,start_date,selected_members,period,latest_n="",end_date=""):
+    base_url = "https://www150.statcan.gc.ca/t1/tbl1/en/dtl!downloadDbLoadingData-nonTraduit.action"
+    pid = pid
+    latestN =  latest_n
+    startDate = start_date
+    endDate = end_date
+    csvLocale = "en"
+    selected_members = selected_members
+
+    #encode selected members
+    encoded_selected_members = urllib.parse.quote(selected_members,safe='')
+
+    # Construct the full URL
+    url = "{}?pid={}&latestN={}&startDate={}&endDate={}&csvLocale={}&selectedMembers={}" \
+        .format(base_url,pid,latestN,startDate,endDate,csvLocale,encoded_selected_members)
+
+    # Print the URL
+    print(url,'\n')
+    
+    # Read the CSV data into a DataFrame
+    df = pd.read_csv(url)
+    
+    # drop extra columns
+    columns_to_drop = ['DGUID', 'COORDINATE', 'UOM', 'SCALAR_FACTOR',
+        'UOM_ID','SCALAR_ID','VECTOR', 'STATUS', 'SYMBOL', 'TERMINATED', 'DECIMALS']
+    
+    df = df.drop(columns=[col for col in columns_to_drop if col in df.columns], axis=1)
+    
+    # convert the 'REF_DATE' column to a datetime object
+    df['REF_DATE']=pd.to_datetime(df['REF_DATE'])
+    
+    # set the 'REF_DATE' column as the index
+    df.set_index('REF_DATE', inplace=True)
+
+    # convert the index to a period index
+    # df.index = df.index.to_period(period)
+    
+    #display df
+    print(df.info(),'\n')
+    print(df.tail())
+    
+    return df
+
+'''
+# Test def create_df_sc
+pid = "1410028701"
+start_date = "20190101"
+selected_members = "[[1,2,3],[1,2,3,4,5,6,7,8,9],[1],[1],[1],[1]]"
+period = 'M'
+df = create_df_sc(pid,start_date,selected_members,period)
+'''
+
+
+

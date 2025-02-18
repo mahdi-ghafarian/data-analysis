ad703f89e16761cba543b0c37eb7dd6e3c6605f8
mahdi-ghafarian <mahdi.ghaf@outlook.com>
2025-02-18 16:02:36 -0600

    modified:   tourism/helper.py 	modified:   tourism/main.ipynb

--- tourism/helper.py
+++ tourism/helper.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 import urllib.parse
+import textwrap
+
 
 # This script demonstrates how to download a CSV file from the Statistics Canada website
 # using the downloadDbLoadingData-nonTraduit.action endpoint.
@@ -40,6 +42,7 @@ def create_df_sc(pid,start_date,selected_members,period,latest_n="",end_date="")
     df.set_index('REF_DATE', inplace=True)
 
     # convert the index to a period index
+    # removed: causes error in seaborn plots
     # df.index = df.index.to_period(period)
     
     #display df
@@ -58,4 +61,12 @@ df = create_df_sc(pid,start_date,selected_members,period)
 '''
 
 
+
+def wrap_labels(ax, width, break_long_words=False):
+    labels = []
+    for label in ax.get_xticklabels():
+        text = label.get_text()
+        labels.append(textwrap.fill(text, width=width,
+                      break_long_words=break_long_words))
+    ax.set_xticklabels(labels, rotation=0)
 
